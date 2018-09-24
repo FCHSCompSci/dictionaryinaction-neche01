b4543e35e17a45dcfca3f87b8e0bfb2124b4cd6d
Echevarria Arroyo <nayeli.echevarriaarroyo@stu.jefferson.kyschools.us>
2018-09-24 08:18:35 -0400

    firrst

---
+++ nayeli.py
@@ -0,0 +1,51 @@
+best_game = {
+   'player':'blank',
+   'gametype':'type',
+   'level':0,
+   'played_by':'blank',
+   'game_state':True,
+   }                                                                                
+
+
+while True:
+    best_game['player'] = input("What is your name?")
+
+
+    print(best_game)
+
+    for key, value in best_game.items():
+        print(f"{key}: {value}")
+
+
+# dice game
+import randow
+
+
+dice_roller ={
+    'name':"bob",
+    'age:25",
+    }
+# a variable that stores a randow dice roll
+dice_roll = 0
+
+while True:
+    # a variable of user_input that stores the value from input()
+    new_name = input ("what is your name")
+    dice_roller['name'] = new_game
+    new_age = input ("what is your age ?")
+    dice_roller['age'] = new_age
+    
+
+    
+    roll_command = input("press enter to roll the dice: ")
+    dice_roll = randow.randint(1,7)
+    print("you rolled a %s " % dice_rool)
+
+
+
+    return True:
+dice_roller 
+
+    
+
+
